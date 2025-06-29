c8cdf5c5e7606a90bdaa36a229acba11b73bab2e
NeoSkosana <skosananeogift@gmail.com>
2025-06-29 21:08:11 +0200

    Complete remove e problem

--- problems/04-remove-e-words-filter.js
+++ problems/04-remove-e-words-filter.js
@@ -13,8 +13,10 @@ console.log(removeEWords('Enter the building')); // 'building'
 */
 
 let removeEWords = function(sentence) {
-    // Your code here
-};
+  return sentence.split(" ").filter(function(word) {
+    return !word.includes("e") && !word.includes("E");
+  }).join(" ");
+}
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
@@ -22,4 +24,4 @@ try {
   module.exports = removeEWords;
 } catch (e) {
   module.exports = null;
-}
+}
