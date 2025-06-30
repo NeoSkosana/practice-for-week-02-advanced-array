817e5141a233f4dd2f1b85f021fe3950e761497f
NeoSkosana <skosananeogift@gmail.com>
2025-06-30 04:21:34 +0200

    Complete smax reduce

--- problems/07-max-reduce.js
+++ problems/07-max-reduce.js
@@ -15,7 +15,14 @@ console.log(maxWithReduce([-2, -3, -7, 3])); // 3
 */
 
 let maxWithReduce = function(nums) {
-    // Your code here
+    return nums.reduce(function(max, current) {
+      if (current > max) {
+        return current;
+      }
+      else {
+        return max;
+      }
+    })
 };
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
